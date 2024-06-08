fdde3662011d811af9d22a692341fdd0fad26304
Janito Vaqueiro Ferreira Filho <janito.vff@linera.io>
2024-06-08 06:10:44 +0000

    Test if gRPC proxy shuts down gracefully
    Ensure that it finishes proxying a request before the process exits.

--- linera-service/tests/shutdown_tests.rs
+++ linera-service/tests/shutdown_tests.rs
@@ -130,6 +130,37 @@ async fn grpc_server_shutdown() -> anyhow::Result<()> {
     Ok(())
 }
 
+/// Check that the gRPC proxy shuts down gracefully.
+///
+/// Ensure that it finishes proxying the response to an active request before the process ends.
+#[test_log::test(tokio::test)]
+async fn grpc_proxy_shutdown() -> anyhow::Result<()> {
+    let _guard = INTEGRATION_TEST_GUARD.lock().await;
+    let config = LocalNetConfig {
+        num_other_initial_chains: 0,
+        num_shards: 1,
+        num_initial_validators: 1,
+        ..LocalNetConfig::new_test(Database::Service, Network::Grpc)
+    };
+    let (mut localnet, _client) = config.instantiate().await?;
+
+    let connection_task = spawn_grpc_connection_task(LocalNet::proxy_port(0) as u16);
+
+    time::sleep(Duration::from_millis(200)).await;
+    localnet.terminate_proxy(0)?;
+
+    localnet.ensure_is_running().await?;
+
+    connection_task.await??;
+    time::sleep(Duration::from_millis(50)).await;
+
+    assert!(localnet.ensure_is_running().await.is_err());
+
+    localnet.reap_proxy(0)?;
+    localnet.terminate().await?;
+    Ok(())
+}
+
 /// Spawns a tasks that connects to the validator and performs a dummy query twice.
 ///
 /// The first query should succeed, even though a termination signal will be sent while it
